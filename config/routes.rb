099d80c1dc96175146d788092d7f5877acd1546f
Jen Hamon <jhamon@gmail.com>
2014-04-23 19:19:32 -0700

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,15 @@
 SimpleShortner::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # Catch and redirect our shortlinks
+  get "/:code" => "page_views#create"
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # A show page for link clickthrough information.
+  resources :shortlinks, :only => [:show]
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # Provide an endpoint for client-side code.
+  namespace :api do
+    resources :target_urls, :only => [:create]
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # Deliver the client-side app.
+  root :to => "static_pages#root"
 end
